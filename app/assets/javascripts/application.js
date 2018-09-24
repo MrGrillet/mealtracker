751e1dc422dc4284fe8283272aac567118a90bb5
Peter <pagrillet@gmail.com>
2018-09-24 09:32:57 +0100

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,69 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= cable
+//= require toastr
+//= require_tree .
+//= flickity.min
+//= easypiechart.min
+//= parallax
+//= typed.min
+//= datepicker
+//= isotope.min
+//= ytplayer.min
+//= lightbox
+//= granim.min
+//= jquery.steps.min
+//= countdown.min
+//= twitterfetcher.min
+//= spectragram.min
+//= smooth-scroll.min
+//= scripts
+
+
+<script src="js/jquery-3.1.1.min.js"></script>
+       <script src="js/flickity.min.js"></script>
+       <script src="js/easypiechart.min.js"></script>
+       <script src="js/parallax.js"></script>
+       <script src="js/typed.min.js"></script>
+       <script src="js/datepicker.js"></script>
+       <script src="js/isotope.min.js"></script>
+       <script src="js/ytplayer.min.js"></script>
+       <script src="js/lightbox.min.js"></script>
+       <script src="js/granim.min.js"></script>
+       <script src="js/jquery.steps.min.js"></script>
+       <script src="js/countdown.min.js"></script>
+       <script src="js/twitterfetcher.min.js"></script>
+       <script src="js/spectragram.min.js"></script>
+       <script src="js/smooth-scroll.min.js"></script>
+       <script src="js/scripts.js"></script>
+
+
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "positionClass": "toast-bottom-left",
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
